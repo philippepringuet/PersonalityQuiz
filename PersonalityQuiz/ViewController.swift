ed904182982a98665ae9144af2810916ad07f0a8
Philippe Pringuet <philippe.pringuet@polytechnic.bh>
2019-03-11 10:24:22 +0300

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Philippe Pringuet on 3/11/19.
+//  Copyright © 2019 Philippe Pringuet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
